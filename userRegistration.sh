ee7fe96fd5ec4966c0168107c36535492567ea3c
bharti mali <malibharti5@gmail.com>
2020-10-23 12:02:28 +0530

    ADD : email pattern

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -x
 read -p "Enter First Name - " firstname
 pattern="^[A-Z]+[a-zA-Z0-9]{2,}"
+emailPattern="^[a-z0-9]{3,}(.[0-9a-z]+)*@[a-z]{2,}.(com|edu)(co.in)*$"
 if [[ $firstname =~ $pattern ]]
 then
 	read -p "Enter Last Name - " lastname
 	if [[ $lastname =~ $pattern ]]
 	then
-	        echo "YES"
+	        read -p "Enter Email - " email
+	        if [[ $email =~ $emailPattern ]]
+        	then
+			echo "yes"
+        	else
+                	echo "Enter valid email"
+        	fi
+
 	else
-        	echo "NO"
+        	echo "Enter the valid last name"
 	fi
 else 
 	echo "Enter a valid name"
