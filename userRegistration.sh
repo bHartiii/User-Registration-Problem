b1e4675a3e88ed7eb6d4dc92ad96534ea58baadc
bharti mali <malibharti5@gmail.com>
2020-10-30 19:42:53 +0530

    MODIFY : Nested if else code removed

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,38 +1,33 @@
 #!/bin/bash -x
 read -p "Enter First Name - " firstname
+read -p "Enter Last Name - " lastname
+read -p "Enter Email - " email
+read -p "Enter Phone No. - " phone
+read -p "Enter Password - " password
+
 pattern="^[A-Z]+[a-zA-Z0-9]{2,}"
 emailPattern="^[a-z0-9]{3,}(.[0-9a-z]+)*@[a-z]{2,}.(com|edu)(co.in)*$"
 phonePattern="^91 [6-9]{1}[0-9]{9}$"
 passwordRule="^[a-z0-9]{3,}(.[0-9a-z]+)*@[a-z0-9]{1,}.([a-z]{2,})(.([a-z]{2,})){0,1}$"
 if [[ $firstname =~ $pattern ]]
 then
-	read -p "Enter Last Name - " lastname
-	if [[ $lastname =~ $pattern ]]
-	then
-	        read -p "Enter Email - " email
-	        if [[ $email =~ $emailPattern ]]
-        	then
-			read -p "Enter Phone No. - " phone
-	                if [[ $phone =~ $phonePattern ]]
-        	        then
-                	   	read -p "Enter Password - " password
-                        	if [[ $password =~ $passwordRule ]]
-                        	then
-                                	echo "yes"
-                        	else
-                                	echo "Not Valid Password "
-                        	fi
-                	else
-                        	echo "Not valid Phone number"
-                	fi
-        	else
-                	echo "Not valid email"
-        	fi
-
-	else
-        	echo "Not valid last name"
-	fi
-else 
-	echo "Not a valid name"
+	echo "First Name correct."
 fi
+if [[ $lastname =~ $pattern ]]
+then 
+	echo "Last Name correct"
+fi
+if [[ $email =~ $emailPattern ]]
+then
+	echo "Email id is correct"
+fi
+if [[ $phone =~ $phonePattern ]]
+then
+	echo "Phone No is correct."
+fi
+if [[ $password =~ $passwordRule ]]
+then
+      	echo "Password is correct"
+fi
+
 
